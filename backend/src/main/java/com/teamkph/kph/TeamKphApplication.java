b718fe2d0c48967edb2dad7cf0b3eab21669bbbd
ip99202 <ip99202@naver.com>
2021-01-08 14:57:05 +0900

    init

---
+++ backend/src/main/java/com/teamkph/kph/TeamKphApplication.java
@@ -0,0 +1,13 @@
+package com.teamkph.kph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamKphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamKphApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/teamkph/kph/TeamKphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamkph.kph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamKphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
