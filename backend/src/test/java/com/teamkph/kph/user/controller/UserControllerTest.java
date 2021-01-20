a9c652a10cd0ae0b8f0c720fbdecf9d7770b8689
ip99202 <ip99202@naver.com>
2021-01-20 16:44:22 +0900

    테스트 코드 작성

--- backend/src/test/java/com/teamkph/kph/user/controller/UserControllerTest.java
+++ backend/src/test/java/com/teamkph/kph/user/controller/UserControllerTest.java
@@ -1,12 +1,49 @@
 package com.teamkph.kph.user.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class UserControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @DisplayName("회원가입 테스트")
     @Test
-    void registration() {
+    public void signupTest() throws Exception {
+        Map<String, String> input = new HashMap<>();
+        input.put("name", "test2");
+        input.put("email", "test2@google.com");
+        input.put("password", "test2_password");
+
+        mockMvc.perform(post("/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(input)))
+                .andExpect(status().isOk())
+                .andDo(print());
     }
+
+//    @DisplayName("일반 로그인 테스트")
+//    @Test
+//    @WithMockUser(username = "mockUser", roles = {"ROLE_USER"})
+//    public void loginTest() throws Exception {
+////        mockMvc.perform(MockMvcRequestBuilders.get())
+//    }
 }
