cf05136a57be20b85e6cb9d3a6b91e688c3beeea
ip99202 <ip99202@naver.com>
2021-02-01 23:11:45 +0900

    Validation DTO 추가

--- backend/src/main/java/com/teamkph/kph/user/dto/SessionUser.java
+++ backend/src/main/java/com/teamkph/kph/user/dto/SessionUser.java
@@ -7,17 +7,32 @@ import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 import java.io.Serializable;
-
-
 public class SessionUser implements Serializable {
+
+
+    @NotBlank
+    @Length(min=3, max=30)
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z_0-9-]")
     private String name;
+
+    @NotBlank
+    @Length(min=8, max=40)
     private String password;
+
+    @NotBlank
+    @Email
     private String email;
 
     @Autowired
-    public PasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;
 
+    @Builder
     public SessionUser(User user) {
         this.name = user.getName();
         this.password = encrypt(user.getPassword());
