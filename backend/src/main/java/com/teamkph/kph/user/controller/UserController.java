1165755aa57bdb2b63b0f7e34a32030ca53afed3
ip99202 <ip99202@naver.com>
2021-02-01 23:09:29 +0900

    Swagger Documentation 작성

--- backend/src/main/java/com/teamkph/kph/user/controller/UserController.java
+++ backend/src/main/java/com/teamkph/kph/user/controller/UserController.java
@@ -1,13 +1,15 @@
 package com.teamkph.kph.user.controller;
 
-import com.teamkph.kph.user.domain.User;
-import com.teamkph.kph.user.domain.dto.UserInfoDto;
-import com.teamkph.kph.user.domain.dto.UserSaveDto;
-import com.teamkph.kph.user.domain.dto.UserUpdateDto;
+import com.teamkph.kph.user.dto.UserInfoDto;
+import com.teamkph.kph.user.dto.UserSaveDto;
+import com.teamkph.kph.user.dto.UserUpdateDto;
 import com.teamkph.kph.user.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,7 +18,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public UserSaveDto registration(@RequestBody UserSaveDto userSaveDto) throws Exception {
+    public UserSaveDto registration(@RequestBody @Valid UserSaveDto userSaveDto) throws Exception {
         return userService.join(userSaveDto);
     }
 
@@ -25,12 +27,14 @@ public class UserController {
         return userService.findUserByEmail(email);
     }
 
+    @ApiOperation(value="사용자 정보 수정", notes="특정 사용자의 정보를 수정합니다다.")
     @PatchMapping("/user/{email}")
     public void fixUserInfo(@PathVariable("email") String email, @RequestBody UserUpdateDto user) throws Exception {
         System.out.println(email);
         userService.fixUserInfo(email, user);
     }
 
+    @ApiOperation(value="사용자 삭제", notes="사용자를 삭제합니다.")
     @DeleteMapping("/user/{email}")
     public void deleteUser(@PathVariable("email") String email) throws Exception {
         userService.deleteUser(email);
