31351be7d495ea96e249b14e01acb31b8638b59c
vividswan <vividswan@naver.com>
2021-02-21 23:17:41 +0900

    feat: about 페이지 삭제 및 rejectAuthUser 함수 생성

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,39 @@
 import Vue from "vue"
 import VueRouter from "vue-router"
-import Home from "../views/Home.vue"
+import store from "@/store"
 
 Vue.use(VueRouter)
+
+const rejectAuthUser = (to, from, next) => {
+  if (store.state.isLogin === true) {
+    alert("이미 로그인 하였습니다.")
+    next("/")
+  } else {
+    next()
+  }
+}
+
+// const onlyAuthUser = (to, from, next) => {
+//   if (store.state.isLogin === false) {
+//     alert("로그인이 필요합니다.")
+//     next("/login")
+//   } else {
+//     next()
+//   }
+// }
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue")
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/login",
+    name: "login",
+    beforeEnter: rejectAuthUser,
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "login" */ "../views/Login.vue")
   }
 ]
 
