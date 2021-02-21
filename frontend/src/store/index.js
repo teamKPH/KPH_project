2a39dc37d0b9863fa9935012f7ad64046da3e6bc
vividswan <vividswan@naver.com>
2021-02-21 23:18:41 +0900

    feat: 로그인 과정에 필요한 vuex store 생성

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,41 @@
 import Vue from "vue"
 import Vuex from "vuex"
+import axios from "axios"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    userInfo: null,
+    isLogin: false
+  },
+  mutations: {
+    loginSuccess(state, payload) {
+      state.isLogin = true
+      state.userInfo = payload
+    },
+    logout(state) {
+      state.isLogin = false
+      state.userInfo = null
+      localStorage.removeItem("access_token")
+    }
+  },
+  actions: {
+    getAccountInfo({ commit }) {
+      let token = localStorage.getItem("access_token")
+      axios
+        .get("/get-account", {
+          headers: {
+            "X-AUTH-TOKEN": token
+          }
+        })
+        .then(response => {
+          commit("loginSuccess", response.data.data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  },
   modules: {}
 })
