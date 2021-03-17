6750a36321c3caeee835e4401b2dff1e1f8ba92c
vividswan <vividswan@naver.com>
2021-03-17 10:26:58 +0900

    fix: cors 에러(front-end -> back-end) 수정

--- backend/src/main/java/com/teamkph/kph/config/SecurityConfig.java
+++ backend/src/main/java/com/teamkph/kph/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.teamkph.kph.registration.auth.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,6 +20,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -26,12 +29,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-//    @Bean
-//    @Override
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins(mappingUrl)
+                    .allowedMethods("GET", "POST", "OPTIONS", "PUT");
+        }
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -59,21 +72,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${mapping.url}")
     private String mappingUrl;
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin(mappingUrl);
-        configuration.addAllowedMethod("*");
-        configuration.addAllowedHeader("*");
-//        configuration.addAllowedHeader("Content-Type");
-//        configuration.addAllowedHeader("Authorization");
-//        configuration.addAllowedHeader("Accept-Language");
-        configuration.setAllowCredentials(true);
-        configuration.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
-    }
 }
